8e985dfe67d11247781c77b30b9f46c6864dadf7
Robson <robsoncouto@outlook.com>
2016-09-14 22:38:43 -0300

    Added constants file

---
+++ constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+#define LED 7
+#define CLK 7
+#define RST 5
+#define WR 4
+#define RD 3
+#define MREQ 2
+
+#define ControlPort PORTD
+#define ControlDDR  DDRD
+#define DATAOUT PORTC
+#define DATAIN PINC
+#define ADRH PORTA
+#define ADRL PORTB
+
+#endif
